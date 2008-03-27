6180a5ddd915e77b6a584c5e6b3c4d42e43d22a2
bwyrosdick <ben.wyrosdick@gmail.com>
2008-03-27 01:15:05 -0500

    switched to returning syntax

--- lib/flickr.rb
+++ lib/flickr.rb
@@ -4,3 +4,10 @@ require 'open-uri'
 include CommonThread::XML
 
 Dir[File.join(File.dirname(__FILE__), 'flickr/**/*.rb')].sort.each { |lib| require lib }
+
+class Object
+  def returning(value)
+    yield(value)
+    value
+  end
+end
