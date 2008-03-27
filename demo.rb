6c70c945e20f14e2419014ecf7ce10e6db2a6c9e
bwyrosdick <ben.wyrosdick@gmail.com>
2008-03-27 02:20:28 -0500

    added PhotoResponse and put pagination helpers in the response

--- demo.rb
+++ demo.rb
@@ -2,4 +2,11 @@ require 'lib/flickr'
 
 flickr = Flickr::Base.new('bb662c756a830975d00b8277bff70ddf')
 
-photos = flickr.photos.search(:tags => 'traingo')
+photos = flickr.photos.search(:tags => 'traingo', :per_page => 10)
+puts photos.inspect
+puts photos.next_page.inspect
+puts photos.previous_page.inspect
+
+while photos = photos.next_page
+  puts photos.page
+end
--- lib/flickr/photos.rb
+++ lib/flickr/photos.rb
@@ -6,12 +6,30 @@ class Flickr::Photos < Flickr::Base
   def search(options)
     rsp = @flickr.send_request('flickr.photos.search', options)
 
-    returning [] do |photos|
+    returning PhotoResponse.new(rsp.photos[:page], rsp.photos[:pages], rsp.photos[:perpage], rsp.photos[:total], [], self, 'flickr.photos.search', options) do |photos|
       rsp.photos.photo.each do |photo|
         photos << Photo.new(photo[:id], photo[:owner], photo[:secret], photo[:server], photo[:farm], photo[:title], photo[:ispublic], photo[:isfriend], photo[:isfamily])
       end
     end
   end
 
+  class PhotoResponse < Struct.new(:page, :pages, :per_page, :total, :photos, :api, :method, :options)
+    def <<(photo)
+      self.photos << photo
+    end
+    
+    def each &block
+      self.photos.each &block
+    end
+    
+    def next_page
+      api.send(self.method.split('.').last, options.merge(:page => self.page.to_i + 1)) if self.page.to_i < self.pages.to_i
+    end
+    
+    def previous_page
+      api.send(self.method.split('.').last, options.merge(:page => self.page.to_i - 1)) if self.page.to_i > 1
+    end
+  end
+  
   class Photo < Struct.new(:id, :owner, :secret, :server, :farm, :title, :is_public, :is_friend, :is_family); end
 end
